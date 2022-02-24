e8a6f1dbf16805f8e68a54ba4a3c005d5211ab1e
VndrGrhrd <vando124@gmail.com>
2022-02-24 09:37:19 -0300

    version 01

---
+++ src/app/main.java
@@ -0,0 +1,38 @@
+package app;
+
+import entities.Products;
+
+import java.util.Locale;
+
+public class main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Products produto = new Products();
+
+        produto.name = "TV";
+        produto.price = 2580.56;
+        produto.quantity = 5;
+
+        System.out.println("Nome: " + produto.name);
+        System.out.println("Preco: " + produto.price);
+        System.out.println("Quantity: " + produto.quantity);
+        System.out.println("Total em Estoque: " + produto.TotalValueStock());
+        System.out.println();
+
+        produto.AddProducts(4);
+        System.out.println("Nome: " + produto.name);
+        System.out.println("Preco: " + produto.price);
+        System.out.println("Quantity: " + produto.quantity);
+        System.out.println("Total em Estoque: " + produto.TotalValueStock());
+        System.out.println();
+
+        produto.RemoveProducts(5);
+        System.out.println("Nome: " + produto.name);
+        System.out.println("Preco: " + produto.price);
+        System.out.println("Quantity: " + produto.quantity);
+        System.out.println("Total em Estoque: " + produto.TotalValueStock());
+        System.out.println();
+
+    }
+}
